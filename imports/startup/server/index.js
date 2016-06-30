216720136e00382c6741f53233092c4449cf2826
Sylvain Duchesne <pro@sylvainduchesne.com>
2016-06-30 12:08:27 +0200

    meteor base app

---
+++ imports/startup/server/index.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+
+//import '../common/useraccounts-configuration.js';
+import '../common/admin-configuration.js';
+import './register-api.js';
+
+if(Meteor.settings.MAIL_URL)
+  process.env.MAIL_URL = Meteor.settings.MAIL_URL;
